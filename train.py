9d4793f1de73abf1d6b50fdfe9554e38b9500da0
Chenchen Gu <63085075+chenchenygu@users.noreply.github.com>
2026-03-11 18:56:59 -0400

    Revert "increase attention softmax_scale (head_dim^-0.25)"
    This reverts commit 73796a2c53a726b381e624e05b403bd3e600226b.

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ class CausalSelfAttention(nn.Module):
         q, k = apply_rotary_emb(q, cos, sin), apply_rotary_emb(k, cos, sin)
         q, k = norm(q), norm(k)
 
-        y = fa3.flash_attn_func(q, k, v, causal=True, window_size=window_size, softmax_scale=self.head_dim**-0.25)
+        y = fa3.flash_attn_func(q, k, v, causal=True, window_size=window_size)
         y = y.contiguous().view(B, T, -1)
         y = self.c_proj(y)
         return y
